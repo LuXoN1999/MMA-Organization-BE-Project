c698c1d60885122868f8de637584ec8414b1f0b7
LuXoN1999 <llovretic99@gmail.com>
2022-11-19 23:59:41 +0100

    Project initialization

---
+++ src/main/java/com/example/MMA/Organization/MmaOrganizationApplication.java
@@ -0,0 +1,13 @@
+package com.example.MMA.Organization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmaOrganizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmaOrganizationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MMA/Organization/MmaOrganizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MMA.Organization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmaOrganizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
