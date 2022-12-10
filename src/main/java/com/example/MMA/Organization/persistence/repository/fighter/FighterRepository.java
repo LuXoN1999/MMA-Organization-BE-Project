4e9c86ebbc67f8dec384a856fc4ec3bffc70dcdd
LuXoN1999 <llovretic99@gmail.com>
2022-12-10 16:20:41 +0100

    Added getAllFightersExceptSpecified method in FighterRepository

--- src/main/java/com/example/MMA/Organization/persistence/repository/fighter/FighterRepository.java
+++ src/main/java/com/example/MMA/Organization/persistence/repository/fighter/FighterRepository.java
@@ -2,9 +2,17 @@ package com.example.MMA.Organization.persistence.repository.fighter;
 
 import com.example.MMA.Organization.persistence.entity.fighter.Fighter;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface FighterRepository extends JpaRepository<Fighter, Long> {
+    @Query(
+            value = "SELECT * FROM fighter f WHERE f.id <> ?#{[0]}",
+            nativeQuery = true
+    )
+    List<Fighter> getAllFightersExceptSpecified(Long fighterIdToIgnore);
 
 }
